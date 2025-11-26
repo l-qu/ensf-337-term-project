48e0fd3a590e5b06fe652170a1f94e40d0dadffd
gianna-k0520 <gianna.kong@ucalgary.ca>
2025-11-26 11:45:43 -0700

    Added Flight class

--- flight_classes.hpp
+++ flight_classes.hpp
@@ -81,6 +81,31 @@ class Route {
         string get_destination()const{return destination;}
         void set_source(string src){source = src;}
         void set_destination(string dest){destination = dest;}
-}
+};
+
+class Flight{
+
+private:
+    string id;
+    int num_rows;
+    int num_cols;
+    vector<Passenger> passengers;
+    vector<vector<Seat>> seat_map;
+
+public:
+
+    Flight(): id(""), num_rows(0), num_cols(0) {}
+    Flight(string id, int r, int c);
+
+    void add_passenger(int id, string fname, string lname, string phone);
+    void remove_passenger(int id);
+    int get_passenger(int id)const;
+    string get_id()const;
+    void show_seat_map()const;
+    void set_set_status(int row, char col, char status);
+    bool check_seat(int row, int col)const;
+    void assign_seat(int row, char col, int passenger_id);
+    vector<Passenger> get_passengers_list() const{return passengers;}
+};
 
 #endif /* flight_classes.hpp */
