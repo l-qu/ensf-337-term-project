a6ba17ef285f39918ecc9c843c8cccdd35d49153
l-qu <232516298+l-qu@users.noreply.github.com>
2025-11-26 12:13:02 -0700

    Added separate files for Airline and Route classes

---
+++ airline.hpp
@@ -0,0 +1,24 @@
+#ifndef AIRLINE_H
+#define AIRLINE_H
+
+#include "flight.hpp"
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Airline {
+    private: 
+        string name;
+        vector<Flight> flights;
+
+    public:
+        Airline(string name);
+        void add_flight(string id, int rows, int cols);
+        void remove_flight(string id);
+        string get_name()const{return name;}
+        int get_flight(string id)const;
+        vector<Flight>& get_flights(){return flights;}
+};
+
+#endif
---
+++ route.hpp
@@ -0,0 +1,20 @@
+#ifndef ROUTE_H
+#define ROUTE_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Route {
+    private: 
+        string source;
+        string destination;
+    public: 
+        string get_source()const{return source;}
+        string get_destination()const{return destination;}
+        void set_source(string src){source = src;}
+        void set_destination(string dest){destination = dest;}
+}
+
+#endif
